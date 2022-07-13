40133b7dc12f0aa5af580d1e61f7d620513e8bcc
James Lincoln <jamesknewy@gmail.com>
2022-07-13 17:29:27 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
+
+#[cfg(test)]
+    mod tests {
+    
+    use super::*;
+    
+}
