f750ca2f65888b0c7df8bf7804cc3ac4b8b8e559
James Lincoln <jamesknewy@gmail.com>
2022-07-13 21:49:49 +0100

    adding integration test and struture refactor

---
+++ tests/health_check.rs
@@ -0,0 +1,22 @@
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("failed to bind address");
+
+    let _ = tokio::spawn(server);
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
