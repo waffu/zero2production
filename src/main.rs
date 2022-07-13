b5f5ff9f65ddf41f5abc99de9f99679f97b54dd8
James Lincoln <jamesknewy@gmail.com>
2022-07-13 20:25:36 +0100

    testing CI pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-        .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
 
 #[cfg(test)]
-    mod tests {
-    
+mod tests {
+
     use super::*;
-    
 }
