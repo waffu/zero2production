e44224a6e50b1b14b6826e9de4091d171f4ecc50
James Lincoln <jamesknewy@gmail.com>
2022-07-14 02:29:35 +0100

    subscribe route and test cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,29 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscriptions(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscriptions))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
